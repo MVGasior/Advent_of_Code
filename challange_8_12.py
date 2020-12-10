b7d47c2cad53f588201f61413c822696c4af6449
Mateusz Gasior <mvgasior@gmail.com>
2020-12-10 23:32:56 +0100

    Finished challange 08_12_2020 10.12.2020 23:33

--- challange_8_12.py
+++ challange_8_12.py
@@ -1,11 +1,12 @@
-#Eighth challange from advent calendar (06.12.2020)
-#Author: Matuesz Gąsior
-
+# Eighth challange from advent calendar (08.12.2020)
+# Author: Matuesz Gąsior
+from copy import deepcopy
 
 moves = []
 for i in open('input_08_12'):
     moves.append(i.replace('\n', '').split(' '))
-moves.append('end')
+
+moves.append(['end', '0'])
 
 
 def game_moves(i=0, acc=0, move_range=[], available_moves=moves):
@@ -14,39 +15,32 @@ def game_moves(i=0, acc=0, move_range=[], available_moves=moves):
         if available_moves[i][0] == 'acc':
             acc += int(available_moves[i][1])
             i += 1
+            return game_moves(i, acc, move_range, available_moves)
         elif available_moves[i][0] == 'jmp':
             i += int(available_moves[i][1])
+            return game_moves(i, acc, move_range, available_moves)
         elif available_moves[i][0] == 'end':
-            return f'end game acc is: {acc}'
+            return acc, 'win'
         else:
             i += 1
-        game_moves(i, acc, move_range)
+            return game_moves(i, acc, move_range, available_moves)
     else:
-        return 888
+        return acc
 
-#in progress
-'''
-def game_improve(available_moves=moves):
-    for move in range(len(available_moves)):
-        working_spaces = available_moves.copy()
-        if available_moves[move][0] == 'jmp':
-            working_spaces[move][0] = 'nop'
-            print(working_spaces)
-            result = game_moves(available_moves=working_spaces)
-            if result is str:
-                return result
-            else:
-                continue
-        elif available_moves[move][0] == 'nop' and available_moves[move][1] != '0':
-            working_spaces[move][0] = 'jmp'
-            print(working_spaces)
-            result = game_moves(available_moves=working_spaces)
-            if result is str:
-                return result
-            else:
-                continue
 
-'''
+def improve_game(opt_move=moves):
+    moves_2 = deepcopy(opt_move)
+    for move in range(len(opt_move)):
+        if opt_move[move][0] == 'jmp':
+            moves_2[move][0] = 'nop'
+            result = game_moves(move_range=[], available_moves=moves_2)
+            if type(result) is tuple:
+                return f"This is wining point acc is: {result[0]}"
+        elif opt_move[move][0] == 'nop' and opt_move[move][1] != '0':
+            moves_2[move][0] = 'jmp'
+            result = game_moves(move_range=[], available_moves=moves_2)
+            if type(result) is tuple:
+                f"This is wining point acc is: {result[0]}"
+        moves_2 = deepcopy(opt_move)
 
-game_improve()
-print(game_improve())
+print(improve_game())
